2f5dc20fcf5e44de9445aa344a17408e13980e0a
napassorn <yui.napassorn.s@gmail.com>
2021-06-28 00:41:41 +0700

    update template footer

--- assets/_template/footer.php
+++ assets/_template/footer.php
@@ -4,7 +4,7 @@
 
             <div class="container clearfix">
                 <div class="col_half">
-                    <?PHP if(isset($setting['name_web'])){ ?>
+                    <?PHP if(!empty($setting['name_web'])){ ?>
                     Copyrights © 2021 All Rights Reserved by <?PHP echo $setting['name_web']; ?>.<br>
                     <?PHP } ?>
                     <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
@@ -12,13 +12,13 @@
 
                 <div class="col_half col_last tright">
                     <div class="fright clearfix">
-                        <?PHP if(isset($setting['facebook'])){ ?>
+                        <?PHP if(!empty($setting['facebook'])){ ?>
                             <a target="_bank" href="<?PHP echo $setting['facebook']; ?>" class="social-icon si-small si-borderless si-facebook">
                                 <i class="icon-facebook"></i>
                                 <i class="icon-facebook"></i>
                             </a>
                         <?PHP } ?>
-                        <?PHP if(isset($setting['twitter'])){ ?>
+                        <?PHP if(!empty($setting['twitter'])){ ?>
                             <a target="_bank" href="<?PHP echo $setting['twitter']; ?>" class="social-icon si-small si-borderless si-twitter">
                                 <i class="icon-twitter"></i>
                                 <i class="icon-twitter"></i>
@@ -27,7 +27,7 @@
                     </div>
 
                     <div class="clear"></div>
-                    <?PHP if(isset($setting['name_web'])){ ?><i class="icon-envelope2"></i> <?PHP echo $setting['email']; ?> <span class="middot">·</span><?PHP } ?> <?PHP if(isset($setting['tel'])){ ?><i class="icon-headphones"></i> <?PHP echo $setting['tel']; ?><?PHP } ?>
+                    <?PHP if(!empty($setting['email'])){ ?><i class="icon-envelope2"></i> <?PHP echo $setting['email']; ?> <span class="middot">·</span><?PHP } ?> <?PHP if(!empty($setting['tel'])){ ?><i class="icon-headphones"></i> <?PHP echo $setting['tel']; ?><?PHP } ?>
                 </div>
 
             </div>
