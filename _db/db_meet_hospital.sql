b63e967f324f9f4f9872f6877efc9b83faadb406
napassorn <yui.napassorn.s@gmail.com>
2021-06-28 19:05:20 +0700

    update db

--- _db/db_meet_hospital.sql
+++ _db/db_meet_hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 27, 2021 at 07:41 PM
+-- Generation Time: Jun 28, 2021 at 02:04 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.12
 
@@ -35,7 +35,7 @@ CREATE TABLE `about` (
   `about_googlemap` text COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'link google map',
   `about_lavel` int(1) NOT NULL COMMENT 'ประเภทเนื้อหา (1= เนื้อหา,2=รูปภาพ, 3= link google map)',
   `about_type` int(1) NOT NULL COMMENT 'ประเภท (1=ข้อมูลเกี่ยวกับองค์กร, 2=แผนผังองค์กร, 3=แผนที่องค์กร, 4=ตารางเวลาการให้บริการ)',
-  `about_status` int(1) NOT NULL COMMENT 'สถานะการใช้งาน [1=แสดง, 0=ซ่อน]	',
+  `about_status` int(1) NOT NULL DEFAULT 1 COMMENT 'สถานะการใช้งาน [1=แสดง, 0=ซ่อน]	',
   `about_updateby` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'อัพเดตข้อมูลโดย',
   `about_updatedate` datetime NOT NULL COMMENT 'วันที่อัพเดต'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='ข้อมูลเกี่ยวกับองค์กร';
@@ -48,6 +48,75 @@ INSERT INTO `about` (`about_id`, `about_content`, `about_content_googlemap`, `ab
 (1, '<p>dddddd</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>svdgdf</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>dfgdfg</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<div style=\"background:#eeeeee; border:1px solid #cccccc; padding:5px 10px\">sgdgdfg</div>\r\n', '', '104378529720210625_220556.png', '<iframe src=\"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7799.138410869049!2d99.858551!3d12.209689!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x51b42d703b0d112c!2z4LmC4Lij4LiH4Lie4Lii4Liy4Lia4Liy4Lil4Liq4Liy4Lih4Lij4LmJ4Lit4Lii4Lii4Lit4LiU!5e0!3m2!1sth!2sth!4v1624635317817!5m2!1sth!2sth\" width=\"400\" height=\"300\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\"></iframe>', 3, 3, 1, 'ผู้ดูแลระบบ', '2021-06-27 23:59:35'),
 (2, '', NULL, '72324455820210626_001543.jpeg', '', 2, 2, 1, 'ผู้ดูแลระบบ', '2021-06-26 00:15:43'),
 (3, '<p>โรงพยาบาลสามร้อยยอด &nbsp;เดิมใช้ชื่อว่า&nbsp; &quot;โรงพยาบาลกุยบุรี&quot; &nbsp;แต่ต่อมาได้เปลี่ยนชื่อเป็น &quot;โรงพยาบาลสามร้อยยอด&quot;&nbsp;&nbsp;ตามสถานที่ตั้งของกิ่งอำเภอสามร้อยยอดเมื่อเดือน ตุลาคม 2539<br />\r\nโรงพยาบาลสามร้อยยอด &nbsp;&nbsp;ตั้งอยู่เลขที่&nbsp;51&nbsp;&nbsp; หมู่ที่ &nbsp;6&nbsp;&nbsp; ตำบลไร่ใหม่ &nbsp;&nbsp;กิ่งอำเภอสามร้อยยอด&nbsp;จังหวัดประจวบคีรีขันธ์&nbsp;มีเนื้อที่ทั้งหมด&nbsp;18&nbsp;ไร่&nbsp;&nbsp; 2&nbsp;&nbsp; งาน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>\r\n\r\n<p>พ.ศ. &nbsp;&nbsp;&nbsp; 2508&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เริ่มจากสถานีอนามัย&nbsp;ชั้น 2<br />\r\nพ.ศ.&nbsp;&nbsp;&nbsp; &nbsp;2518&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ยกฐานะเป็นศูนย์การแพทย์ และอนามัย<br />\r\nพ.ศ.&nbsp;&nbsp;&nbsp; &nbsp;2519&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ยกฐานะเป็นโรงพยาบาลอำเภอ&nbsp;ขนาด&nbsp;10&nbsp;เตียง<br />\r\nพ.ศ. &nbsp;&nbsp;&nbsp; 2527&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ยกฐานะเป็นโรงพยาบาล&nbsp;ขนาด 60 เตียง<br />\r\nพ.ศ. &nbsp;&nbsp;&nbsp; 2539&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เปลี่ยนชื่อโรงพยาบาลกุยบุรี มาเป็น&nbsp;โรงพยาบาลสามร้อยยอด</p>\r\n\r\n<h3>รายนามผู้อำนวยการโรงพยาบาลสามร้อยยอด</h3>\r\n\r\n<ul>\r\n	<li>นายแพทย์วิทยา&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; คุณานุกูล&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2518-2529</li>\r\n	<li>นายแพทย์พิภพ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; เจนสุทธิเวชกุล&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2529-2532</li>\r\n	<li>นายแพทย์สมเกียรติ&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ตั้งใจรักษาการดี&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2532-2536(รก.)</li>\r\n	<li>นายแพทย์สมชาญ&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; พงษ์ริยวัฒนา&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2536-2539(รก.)</li>\r\n	<li>แพทย์หญิงศรีสมัย&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; เชื้อชาติ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2539-2543(รก.)</li>\r\n	<li>นายแพทย์ปิยะ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ลินลาวรรณ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; พ.ศ.2543-2546(รก.)</li>\r\n	<li>แพทย์หญิงศรีสมัย&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; เชื้อชาติ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2546-2551</li>\r\n	<li>นายแพทย์สมเกียรติ&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;ตั้งใจรักษาการดี&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; พ.ศ.2551-30 ก.ย.2562</li>\r\n	<li>แพทย์หญิงวศินี&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; วีระไวทยะ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;1 ต.ค.2562-ปัจจุบัน</li>\r\n</ul>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<h3><strong>วิสัยทัศน์ (VISION) ปี 2560 &ndash; 2564</strong></h3>\r\n\r\n<p>เป็นผู้นำให้บริการด้านสุขภาพที่มีคุณภาพมาตรฐาน บริหารจัดการตามหลักธรรมาภิบาล ผู้รับบริการมีพฤติกรรมสุขภาพที่ดี</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<h3><strong>พันธกิจ (MISSION) ปี 2560 &ndash; 2564</strong></h3>\r\n\r\n<ol>\r\n	<li>พัฒนาระบบบริการจัดการด้านสุขภาพให้มีประสิทธิภาพตามหลักธรรมาภิบาล</li>\r\n	<li>พัฒนาคุณภาพมาตรฐานด้านการรักษาพยาบาลการเฝ้าระวังป้องกันโรค&nbsp;&nbsp; การส่งเสริมสุขภาพ&nbsp; และฟื้นฟูสภาพ</li>\r\n	<li>ส่งเสริมและพัฒนาการใช้ภูมิปัญญาการแพทย์แผนไทยและการแพทย์ทางเลือก</li>\r\n	<li>สร้างเสริมพฤติกรรมสุขภาพที่ถูกต้องของประชาชน</li>\r\n	<li>พัฒนาระบบประกันสุขภาพให้ประชาชนเข้าถึงบริการได้อย่างเสมอภาคและเป็นธรรม</li>\r\n	<li>ส่งเสริมและจัดระบบการคุ้มครองผู้บริโภค</li>\r\n	<li>เสริมสร้างความเข้มแข็งของภาคีเครือข่าย</li>\r\n	<li>เสริมสร้างและพัฒนาสมรรถนะบุคลากร</li>\r\n	<li>พัฒนาระบบข้อมูลข่าวสาร&nbsp; เทคโนโลยีสารสนเทศให้มีประสิทธิภาพ</li>\r\n	<li>พัฒนาวิชาการและส่งเสริมการสร้างนวัตกรรมด้านสุขภาพ</li>\r\n</ol>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<h3>ยุทธศาสตร์</h3>\r\n\r\n<ol>\r\n	<li>พัฒนาระบบบริหารจัดการภายในองค์กรและเครือข่ายให้สอดคล้องตามหลักธรรมาภิบาล</li>\r\n	<li>พัฒนาและจัดการระบบการรักษาพยาบาลเฝ้าระวังป้องกันโรค&nbsp; ส่งเสริมสุขภาพ&nbsp; และฟื้นฟุสภาพ</li>\r\n	<li>พัฒนาระบบการแพทย์แผนไทยและการแพทย์ทางเลือก</li>\r\n	<li>ส่งเสริมการจัดบริการสร้างเสริมพฤติกรรมการกูแลสุขภาพ</li>\r\n	<li>ส่งเสริมการจัดระบบบริการสุขภาพที่ประชาชนสามารถเข้าถึงได้อย่างครอบคลุมและทั่วถึง</li>\r\n	<li>พัฒนากลไกลการดำเนินงานควบคุม&nbsp; กำกับด้านคุ้มครองผู้บริโภคและด้านบริการสุขภาพ</li>\r\n	<li>&nbsp;พัฒนาภาคีเครือข่ายให้มีศักยภาพและส่งเสริมการมีส่วนร่วมในการดำเดินงาสร้างสุ๘ขภาพ</li>\r\n	<li>พัฒนาศักยภาพบุคลากร</li>\r\n	<li>พัฒนาระบบข้อมูลสารสนเทศ</li>\r\n	<li>พัฒนาองค์กรให้เป็นองค์กรแห่งการเรียนรู้</li>\r\n</ol>\r\n\r\n<p>&nbsp;</p>\r\n', NULL, '', '', 1, 1, 1, 'ผู้ดูแลระบบ', '2021-06-26 00:03:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `album_id` int(11) NOT NULL,
+  `album_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'ชื่ออัลบั้ม',
+  `album_detail` text COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'รายละเอียด',
+  `album_status` int(1) NOT NULL DEFAULT 1 COMMENT 'สถานะการใช้งาน [1=แสดง, 0=ซ่อน]	',
+  `album_addby` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'เพิ่มข้อมูลโดย',
+  `album_adddate` datetime DEFAULT NULL COMMENT 'วันที่เพิ่มข้อมูล',
+  `album_updateby` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'อัพเดตข้อมูลโดย',
+  `album_updatedate` datetime DEFAULT NULL COMMENT 'วันที่อัพเดต'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`album_id`, `album_title`, `album_detail`, `album_status`, `album_addby`, `album_adddate`, `album_updateby`, `album_updatedate`) VALUES
+(4, 'TEST 1', 'The standard Lorem Ipsum passage, used since the 1500s', 1, 'ผู้ดูแลระบบ', '2021-06-28 17:42:08', 'ผู้ดูแลระบบ', '2021-06-28 17:42:08'),
+(5, 'TEST2', 'The standard Lorem Ipsum passage, used since the 1500s2', 1, 'ผู้ดูแลระบบ', '2021-06-28 18:06:50', 'ผู้ดูแลระบบ', '2021-06-28 18:06:50'),
+(6, 'TEST 3', 'The standard Lorem Ipsum passage, used since the 1500s2', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:07:13', 'ผู้ดูแลระบบ', '2021-06-28 18:07:13'),
+(7, 'TEST 4', 'The standard Lorem Ipsum passage, used since the 1500s2', 1, 'ผู้ดูแลระบบ', '2021-06-28 18:07:39', 'ผู้ดูแลระบบ', '2021-06-28 18:07:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album_des`
+--
+
+CREATE TABLE `album_des` (
+  `des_id` int(11) NOT NULL,
+  `albumId` int(11) NOT NULL COMMENT 'รหัสอัลบั้ม',
+  `des_img` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'ชื่อรูปภาพ',
+  `des_status` int(1) NOT NULL DEFAULT 0 COMMENT 'ภาพหน้าปก [1=ภาพหน้าปก, 0=ภาพปกติ]	',
+  `des_addby` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'เพิ่มข้อมูลโดย',
+  `des_adddate` datetime DEFAULT NULL COMMENT 'วันที่เพิ่มข้อมูล'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `album_des`
+--
+
+INSERT INTO `album_des` (`des_id`, `albumId`, `des_img`, `des_status`, `des_addby`, `des_adddate`) VALUES
+(30, 4, '20210628_1742161.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 17:42:16'),
+(31, 4, '20210628_1742162.jpg', 1, 'ผู้ดูแลระบบ', '2021-06-28 17:42:16'),
+(33, 5, '20210628_1806576.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:06:57'),
+(34, 5, '20210628_1806577.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:06:57'),
+(35, 5, '20210628_1806578.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:06:57'),
+(36, 5, '20210628_1806579.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:06:57'),
+(37, 5, '20210628_18065710.jpg', 1, 'ผู้ดูแลระบบ', '2021-06-28 18:06:57'),
+(38, 6, '20210628_18072911.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:07:29'),
+(39, 6, '20210628_18072912(24).jpg', 1, 'ผู้ดูแลระบบ', '2021-06-28 18:07:29'),
+(40, 7, '20210628_1807474.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:07:47'),
+(41, 7, '20210628_1807475.jpg', 1, 'ผู้ดูแลระบบ', '2021-06-28 18:07:47'),
+(42, 7, '20210628_1807479.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:07:47'),
+(43, 7, '20210628_18074710.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:07:47'),
+(44, 4, '20210628_1859126.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:12'),
+(45, 4, '20210628_1859127.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:12'),
+(46, 4, '20210628_1859128.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:12'),
+(47, 4, '20210628_1859129.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:12'),
+(48, 4, '20210628_18591210.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:12'),
+(49, 4, '20210628_18591911.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:19'),
+(50, 4, '20210628_18591912(24).jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:19'),
+(51, 4, '20210628_18591920593.jpg', 0, 'ผู้ดูแลระบบ', '2021-06-28 18:59:19');
 
 -- --------------------------------------------------------
 
@@ -82,7 +151,7 @@ CREATE TABLE `news` (
   `news_img_cover` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'ภาพหน้าปก',
   `news_title` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'หัวข้อ',
   `news_content` longtext COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'เนื้อหา',
-  `news_status` int(1) NOT NULL COMMENT 'สถานะการใช้งาน [1=แสดง, 0=ซ่อน]',
+  `news_status` int(1) NOT NULL DEFAULT 1 COMMENT 'สถานะการใช้งาน [1=แสดง, 0=ซ่อน]',
   `news_addby` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'เพิ่มข้อมูลโดย',
   `news_adddate` datetime DEFAULT NULL COMMENT 'วันที่เพิ่มข้อมูล',
   `news_updateby` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'อัพเดตข้อมูลโดย',
@@ -137,6 +206,19 @@ ALTER TABLE `about`
   ADD PRIMARY KEY (`about_id`);
 
 --
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`album_id`);
+
+--
+-- Indexes for table `album_des`
+--
+ALTER TABLE `album_des`
+  ADD PRIMARY KEY (`des_id`),
+  ADD KEY `album_des_albumId_index` (`albumId`) USING BTREE;
+
+--
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
@@ -165,10 +247,22 @@ ALTER TABLE `about`
   MODIFY `about_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัส', AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `album_des`
+--
+ALTER TABLE `album_des`
+  MODIFY `des_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสผู้ใช้';
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสผู้ใช้', AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `news`
@@ -181,6 +275,16 @@ ALTER TABLE `news`
 --
 ALTER TABLE `setting`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album_des`
+--
+ALTER TABLE `album_des`
+  ADD CONSTRAINT `album_des_albumId_foreign` FOREIGN KEY (`albumId`) REFERENCES `album` (`album_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
