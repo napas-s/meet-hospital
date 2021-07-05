0fa26ea2d8d8adc5c7c4d10b2742ea132c0b6c0e
napassorn <yui.napassorn.s@gmail.com>
2021-07-06 01:19:33 +0700

    update template

--- assets/_template/footerjs.php
+++ assets/_template/footerjs.php
@@ -3,6 +3,10 @@
     <script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/plugins.js"></script>
     <script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/functions.js"></script>
 
+    <!-- Date & Time Picker JS -->
+	<script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/components/moment.js"></script>
+	<script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/datepicker.js"></script>
+
     <!-- นำเข้าไฟล์ js -->
     <script src="<?PHP echo base_url(); ?>assets/js/function.js"></script>
 
--- assets/_template/headder.php
+++ assets/_template/headder.php
@@ -39,6 +39,10 @@
 	<link rel="stylesheet" href="<?PHP echo base_url(); ?>assets/vendor/canvas/css/colors.css" type="text/css" />
 	<link rel="stylesheet" href="<?PHP echo base_url(); ?>assets/vendor/canvas/css/fonts.css" type="text/css" />
 
+    <!-- Date & Time Picker CSS -->
+	<link rel="stylesheet" href="<?PHP echo base_url(); ?>assets/vendor/canvas/css/components/datepicker.css" type="text/css" />
+	<link rel="stylesheet" href="<?PHP echo base_url(); ?>assets/vendor/canvas/css/components/timepicker.css" type="text/css" />
+	<link rel="stylesheet" href="<?PHP echo base_url(); ?>assets/vendor/canvas/css/components/daterangepicker.css" type="text/css" />
 
 </head>
 <body class="stretched">
