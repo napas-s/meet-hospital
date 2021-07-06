48d8be0a16e2413370471b354ee57f8f89815014
napassorn <yui.napassorn.s@gmail.com>
2021-07-07 00:05:25 +0700

    update template

--- assets/_template/footerjs.php
+++ assets/_template/footerjs.php
@@ -7,6 +7,10 @@
 	<script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/components/moment.js"></script>
 	<script type="text/javascript" src="<?PHP echo base_url(); ?>assets/vendor/canvas/js/datepicker.js"></script>
 
+    <!-- นำเข้า select2 จากภายนอก -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
     <!-- นำเข้าไฟล์ js -->
     <script src="<?PHP echo base_url(); ?>assets/js/function.js"></script>
 
