1090eb10ad72af4faada027a11f1d4edc56e6fe9
napassorn <yui.napassorn.s@gmail.com>
2021-07-15 00:59:38 +0700

    update ajax date

--- _ajax/serviceDate.php
+++ _ajax/serviceDate.php
@@ -2,12 +2,13 @@
 
     // นำเข้าไฟล์ฐานข้อมูล
     require_once('../_database/connection.php');
+	date_default_timezone_set('Asia/Bangkok');
 
     $serpointId = $_GET['setpointId'];
     $sertypeId = $_GET['settypeId'];
     $dateNow = date('Y-m-d');
 
-    $sql = "SELECT serdes_id,serpoint_id,sertype_id,serdes_date FROM services_des WHERE serpoint_id = $serpointId AND sertype_id = $sertypeId AND serdes_date >= $dateNow ORDER BY serdes_date ASC";
+    $sql = "SELECT serdes_id,serpoint_id,sertype_id,serdes_date FROM services_des WHERE serpoint_id = $serpointId AND sertype_id = $sertypeId AND serdes_date >= '$dateNow' ORDER BY serdes_date ASC";
     $sql_query = mysqli_query($con,$sql)or die(mysqli_error($con));
 
     echo "<option value=''>กรุณาเลือกวันที่นัดหมาย</option>";
